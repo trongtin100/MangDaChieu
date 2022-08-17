625e9751f456ab67608eb91aa193a7c1a28cefc9
Admin <trongtin100@gmail.com>
2022-08-17 22:52:17 +0700

    Ex-04-MangDaChieu

---
+++ EX-03-MangDaChieu.js
@@ -0,0 +1,7 @@
+let value = Number( prompt ("Nhap: "));
+let Numbers = [ -3, 5, 1, 3, 2, 10];
+for (let i = 0 ; i < Numbers.length; i++){
+    if (value == Numbers[i]) {
+        alert("Gia Tri La" + Numbers [i] + "Gia Tri La" + i);
+    }
+}
---
+++ EX-05-CoCaRo.js
@@ -0,0 +1,14 @@
+let b = document.getElementById("carogame");
+
+let board = [];
+let data = "";
+for (let i = 0; i < 5; i ++){
+    board[i] = new Array(0, 0, 0, 0, 0, 0);
+}
+for (let i = 0 ; i < 5; i ++) {
+    data += "<br/>";
+    for (let j = 0 ; j<5; j++) {
+        data += board [i][j] + "&nbsp;&nbsp;";
+    }
+}
+data += "<br/><br/><input type='butoon' value "
